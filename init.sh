28af63415d380cfee202872d1db21077ba264a51
Karol <macbook@MacBook-Air-MacBook.local>
2019-05-26 15:32:15 +0200

    Fix sources

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
 wget https://raw.githubusercontent.com/tetrash/SO/master/assets/sources.list -P /etc/apt/
 
 apt-get update && apt-get install -y git
